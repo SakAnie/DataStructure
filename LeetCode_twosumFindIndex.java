4010e95f5771a1b19a58db1f1f85d6c3fd629e56
Sakshi Sinha <97251562+SakAnie@users.noreply.github.com>
2024-12-20 12:49:06 +0530

    Update LeetCode_twosumFindIndex.java

--- LeetCode_twosumFindIndex.java
+++ LeetCode_twosumFindIndex.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         LinkedHashMap<Integer, Integer> hm = new LinkedHashMap<Integer, Integer>();
+	// to save space!! since its two sum
         int[] keysetArray=new int[2]; 
       
         for (int i = 0; i < nums.length; i++) {
@@ -19,7 +20,7 @@ class Solution {
                     System.out.println(x);
         
             }
-            hm.put(nums[i], i);
+            hm.put(nums[i], i);//must add after the logic not before!!!!
         
         
 
