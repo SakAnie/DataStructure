2c5c682b2a58494149246b261b66f1f3a63f343e
Sakshi Sinha <97251562+SakAnie@users.noreply.github.com>
2024-12-20 12:48:23 +0530

    Update LeetCode_twosumFindIndex.java

--- LeetCode_twosumFindIndex.java
+++ LeetCode_twosumFindIndex.java
@@ -1,58 +1,30 @@
+import java.util.LinkedHashMap;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-
-public class twosumFindIndex {
-
-	public static void main(String[] args) {
-
-		int[] A = { 2, 3, 4, 5, 6, 7, 8, 9, 1 };
-		int low = -1;
-		int high = -1;
-		int key_sum = 10;
-		int curr_sum = 0;
-		int size = A.length;
-
-		LinkedHashMap<Integer, Integer> hm = new LinkedHashMap<Integer, Integer>();
-		// in Key part store the ARRAY value and the value part , store the Array INDEX
-
-		for (int i = 0; i < size; i++) {
-
-			hm.put(A[i], i);
-
-			if (hm.containsKey(key_sum - A[i])&& A[i]!=key_sum-A[i]){
-				
-				//assuming array does not contain duplicates
-				System.out.println("indexes are" + hm.get(A[i]) + "and" + hm.get(key_sum - A[i]));
-			}
-
-		}
-
-	}
-
-}
----- LEETCODE-----
-	import java.util.HashMap;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a map to store numbers and their indices
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        
-        // Iterate through the array to find the two numbers
+        LinkedHashMap<Integer, Integer> hm = new LinkedHashMap<Integer, Integer>();
+        int[] keysetArray=new int[2]; 
+      
         for (int i = 0; i < nums.length; i++) {
-           
-            // Check if the complement is already in the map
-            if (hm.containsKey(target - nums[i])) {
-                // If complement found, return the indices of the two numbers
-                return new int[] { hm.get(target - nums[i]), i };
-            }
+                	
 
-            // If complement not found, store the current number and its index in the map
-            hm.put(nums[i], i);
-        }
+			if (hm.containsKey(target - nums[i])){
+
+                keysetArray[0]=i;
+                keysetArray[1]=hm.get(target-nums[i]);
+
+                for (int x : keysetArray) 
+                    System.out.println(x);
         
-        // If no solution is found (though the problem guarantees one)
-        return new int[] {-1, -1};
+            }
+            hm.put(nums[i], i);
+        
+        
+
+        
+        } 
+        return keysetArray;
     }
 }
